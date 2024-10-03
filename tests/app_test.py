ddb0a9e53c92ea3defa89fa7ac14f2f3f15ca2ec
annechoww <anne.chow123@hotmail.com>
2024-10-03 15:21:55 -0400

    Updated search to test for empty search

--- tests/app_test.py
+++ tests/app_test.py
@@ -97,7 +97,14 @@ def test_search(client):
     db.session.add(post1)
     db.session.add(post2)
     db.session.commit()
+    
+    # Test with query 
     query_param = "First"
     rv = client.get(f"/search/?query={query_param}")
     assert b"First" in rv.data
+    assert b"Second" not in rv.data
+
+    # Test empty search
+    rv = client.get(f"/search/?query=")
+    assert b"First" not in rv.data
     assert b"Second" not in rv.data
